6f8f2489e8189a88bae71d06a6ef26f81358801a
Hungtv2951 <hungtv290501@gmail.com>
2022-01-12 09:45:08 +0700

    news

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ const Menu = {
             <ul class="menu-item">
                 <li><a href="/">Trang chủ</a></li>
                 <li><a href="/about">Tuyển sinh</a></li>
-                <li><a href="">Chương trình đào tạo</a></li>
+                <li><a href="/news">Chương trình đào tạo</a></li>
                 <li><a href="">Góc sinh viên</a></li>
                 <li><a href="">Tuyển dụng</a></li> 
         </ul>
